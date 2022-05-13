bd7e0316c0a1be0baa535eee2d002fc34e967c38
Jonas Ng <75115433+jonas9898@users.noreply.github.com>
2022-05-13 08:39:55 +0200

    Initial commit

--- uni/StringTree.java
+++ uni/StringTree.java
@@ -6,16 +6,6 @@ public class StringTree {
 
     public StringTree(String str) {
         this.savedValue = str;
-//        if (leftChild != null && rightChild != null){
-//            leftChild.toString();this.toString();rightChild.toString();
-//        }
-//        else if (leftChild != null){
-//            leftChild.toString();this.toString();
-//        }
-//        else if (rightChild != null){
-//            this.toString();rightChild.toString();
-//        }
-//        else this.toString();
     }
 
     public void insert(String str) {
@@ -24,82 +14,52 @@ public class StringTree {
 
             if (leftChild == null) {
                 leftChild = new StringTree(str);
-                leftChild.toString();this.toString();
-
             } else {
                 leftChild.insert(str);
-                leftChild.toString();this.toString();
             }
 
         } else {
-
             if (rightChild == null) {
                 rightChild = new StringTree(str);
-                this.toString();rightChild.toString();
-
             } else {
                 rightChild.insert(str);
-                this.toString();rightChild.toString();
             }
         }
     }
+    public String toStringPreOrder() {
+        String s = "";
+        s += savedValue;
+        if (leftChild != null) {
+            s += leftChild.toString();
+        }
+        if (rightChild != null) {
+            s += rightChild.toString();
+        }
+        return s;
 
+    }
 
-    //    public String toStringPreOrder(){
-//        return ;
-//    }
-//    public String toStringPostOrder(){
-//        return ;
-//    }
+    public String toStringPostOrder() {
+        String s = "";
+        if (leftChild != null) {
+            s += leftChild.toString();
+        }
+        if (rightChild != null) {
+            s += rightChild.toString();
+        }
+        s += savedValue;
+        return s;
 
+    }
     public String toString() {
-//        if (leftChild != null && rightChild != null){
-//            return leftChild.savedValue + savedValue + rightChild.savedValue;
-//        }
-//        else if (leftChild != null){
-//            return leftChild.savedValue + savedValue;
-//        }
-//        else if (rightChild != null){
-//            return savedValue + rightChild.savedValue;
-//        }
-        return savedValue;
+        String s = "";
+        if (leftChild != null) {
+            s += leftChild.toString();
+        }
+        s += savedValue;
+        if (rightChild != null) {
+            s += rightChild.toString();
+        }
+        return s;
     }
 }
-
-/**
- * Think about how it should work and then implement the method insert(String s) so that
- * it correctly places the Strings. The value of savedValue will only be settable upon creation
- * of the object via constructor. Meaning for the insert method that it will not save the date
- * in this instance but rather create a new leaf (new StringTree) or delegate the saving to
- * an already existing child instance depending on whether the object to be saved is smaller
- * or bigger than the saved one. Advice: To determine whether a string come before or after
- * another you can use the compareTo(String s) method of the String class.
- * Example:
- * String a = "Ha";
- * String b = "Hi";
- * int result = a.compareTo(b);
- * System.out.println(result); // output: -8
- * • Since a is „smaller“ than b, a.compareTo(b) will return a value smaller than 0.
- * • If a and b were equal, calling a.compareTo(b) would return the value 0.
- * • If a were bigger than b, calling a.compareTo(b) would return a value bigger than 0.
- * <p>
- * <p>
- * c) Implement the method toString in the class StringTree, so that it will print out the textual
- * representation of the tree “in order“ (first the left child, than the actual value and then the right
- * child) Optional: Implement the methods toStringPreOrder() and toStringPostOrder() so
- * that they will print out the according traversal order of the tree.
- * <p>
- * d) Use the program StringTreeDemo from the ILIAS-System to test your class.
- * The print out should look like this:
- * E
- * E G
- * A E G
- * A E G K
- * A C E G K
- * A B C E G K
- * A B C E E G K
- * A B C D E E G K
- * A B C D E E G I K
- * A B C D E E G H I K
- * A B C D E E F G H I K
- */
--- uni/StringTreeDemo.java
+++ uni/StringTreeDemo.java
@@ -10,6 +10,8 @@ public class StringTreeDemo
     public static void main (String[] args)
     {
        StringTree tree = null;
+        StringTree tree2 = null;
+        StringTree tree3 = null;
        
 	   String[] wordsToBeSorted = {"G", "A", "K", "C", "B", "E", "D", "I", "H", "F" }; //must be of lenght at least 9
 	   
@@ -20,6 +22,22 @@ public class StringTreeDemo
 			tree.insert(wordsToBeSorted[i]);
 			System.out.println(tree);
 		}
+
+        tree2 = new StringTree("E");
+        System.out.println(tree2.toStringPreOrder());
+
+        for(int i = 0; i < wordsToBeSorted.length; i++){
+            tree2.insert(wordsToBeSorted[i]);
+            System.out.println(tree2.toStringPreOrder());
+        }
+
+        tree3 = new StringTree("E");
+        System.out.println(tree3.toStringPostOrder());
+
+        for(int i = 0; i < wordsToBeSorted.length; i++){
+            tree3.insert(wordsToBeSorted[i]);
+            System.out.println(tree3.toStringPostOrder());
+        }
 
 		
     }
